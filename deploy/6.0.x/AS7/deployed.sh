173f0b59c79dd1fd8eb311140ada934958755e15
kiereleaseuser <kiereleaseuser@gmail.com>
2014-03-19 12:58:15 +0100

    added 6.0.x deploy for AS7

---
+++ deploy/6.0.x/AS7/deployed.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+LOG_DIR=/home/jbpm-6-0-deploy-as7/jboss-as-7.1.1.Final/standalone/log
+File=$LOG_DIR/server.log
+
+cd $LOG_DIR
+
+grep -w "Failed to start service jboss.deployment.unit." $File > log.txt
+
+if [ $? -eq 0 ]
+then
+    # not deployed
+    echo "6.0.x: kie-wb NOT DEPLOYED on AS7"  | mail -s "Deployment on AS7" -a server.log mbiarnes@redhat.com pzapataf@redhat.com 
+else
+    # deployed
+    echo "6.0.x: kie-wb DEPLOYED on AS7"  | mail -s "Deployment on AS7" mbiarnes@redhat.com 
+fi
