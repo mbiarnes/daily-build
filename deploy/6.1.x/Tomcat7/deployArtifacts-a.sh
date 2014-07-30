1ed04344c5ea1c7d0278b2877cfa7850b342d75e
kiereleaseuser <kiereleaseuser@gmail.com>
2014-07-30 17:07:12 +0200

    fixed tomcat deployment

--- deploy/6.1.x/Tomcat7/deployArtifacts-a.sh
+++ deploy/6.1.x/Tomcat7/deployArtifacts-a.sh
@@ -10,15 +10,6 @@ cd $CONFIG
 # Copy deployments at tomcat7/webapps
 cp $DEFAULT_DIR/kie-wb-*-tomcat7.war $TOMCAT_HOME/webapps/kie-wb.war
 
-
-zip -d $TOMCAT_HOME/webapps/kie-wb.war WEB-INF/web.xml
-zip -d $TOMCAT_HOME/webapps/kie-wb.war WEB-INF/beans.xml
-#zip -d $TOMCAT_HOME/webapps/kie-wb.war WEB-INF/web.xml WEB-INF/classes/META-INF/services/org.uberfire.security.auth.AuthenticationSource
-/usr/java/default/bin/jar -uf $TOMCAT_HOME/webapps/kie-wb.war WEB-INF/web.xml
-/usr/java/default/bin/jar -uf $TOMCAT_HOME/webapps/kie-wb.war WEB-INF/beans.xml
-#/usr/java/default/bin/jar -uf $TOMCAT_HOME/webapps/kie-wb.war WEB-INF/classes/META-INF/services/org.uberfire.security.auth.AuthenticationSource.ORIGIN
-#/usr/java/default/bin/jar -uf $TOMCAT_HOME/webapps/kie-wb.war WEB-INF/classes/META-INF/services/org.uberfire.security.auth.AuthenticationSource
-
 chmod 755 $TOMCAT_HOME/webapps/kie-wb.war
 
 $TOMCAT_HOME/bin/startup.sh&
