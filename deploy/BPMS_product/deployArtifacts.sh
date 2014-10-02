b9732cb4e19ee8d3580fa63b3a50628eb9e5dcd7
kiereleaseuser <kiereleaseuser@gmail.com>
2014-10-02 19:08:35 +0200

    updated BPMS

--- deploy/BPMS_product/deployArtifacts.sh
+++ deploy/BPMS_product/deployArtifacts.sh
@@ -2,12 +2,12 @@
 #
 # Variable definition 
 export DEFAULT_DIR=/var/jbpm-artifacts/6.1.x/
-export JBOSS_HOME=$HOME/jboss-eap-6.1
+export JBOSS_HOME=$HOME/jboss-eap-6.3
 export PERMANENT=$HOME/scripts/config
 
-wget http://dev138.mw.lab.eng.bos.redhat.com/candidate/bpms-6.0.0-ER7/jboss-bpms-6.0.0-redhat-7-deployable-eap6.x.zip
+wget http://dev138.mw.lab.eng.bos.redhat.com/candidate/bpms-6.1.0-DR3/jboss-bpms-6.1.0.redhat-2-deployable-eap6.x.zip
 
-unzip -o jboss-bpms-6.0.0-redhat-7-deployable-eap6.x.zip -d /home/bpms-prod-eap-6.1.1/
+unzip -o jboss-bpms-6.1.0.redhat-2-deployable-eap6.x.zip  -d /home/bpms-prod-eap-6.1.1/
 
 cp $PERMANENT/*.properties $JBOSS_HOME/standalone/configuration
 cp $PERMANENT/standalone-full.xml $JBOSS_HOME/standalone/configuration
--- deploy/BPMS_product/reinstall-eap.sh
+++ deploy/BPMS_product/reinstall-eap.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 export PERMANENT=$HOME/scripts/config
 export RESOURCES=$HOME/scripts/resources
-export JBOSS_HOME=$HOME/jboss-eap-6.1
+export JBOSS_HOME=$HOME/jboss-eap-6.3
 export SCRIPTS=$HOME/scripts/
-
+export OLDZIP=jboss-bpms-6.1.0.redhat-2-deployable-eap6.x.zip
 pkill -9 java
 killall -9 /usr/java/default/bin/java
 
 sleep 15 
 
-rm -rf $HOME/jboss-eap-6.1
+rm -rf $HOME/jboss-eap-6.3
 rm -rf $HOME/Uninstaller
+rm $SCRIPTS/$OLDZIP
 rm $HOME/bpms-prod*.*
 rm $HOME/installProd*
 rm $HOME/jBoss*
@@ -21,6 +22,6 @@ rm -rf $SCRIPTS/.niogit
 rm -rf $SCRIPTS/.index
 rm -rf $SCRIPTS/.security
 
-java -jar $RESOURCES/jboss-eap-6.1.1-installer.jar $PERMANENT/install-eap6.1.1.xml
-# unzip $RESOURCES/jboss-eap.zip -d $HOME
+# java -jar $RESOURCES/jboss-eap-6.1.1-installer.jar $PERMANENT/install-eap6.1.1.xml
+ unzip $RESOURCES/jboss-eap.zip -d $HOME
 
