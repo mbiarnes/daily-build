45f5d09c1e73316e749d1a0e4c88e21caf996092
kiereleaseuser <kiereleaseuser@gmail.com>
2014-10-14 09:44:03 +0200

    all deleted

--- deploy/6.1.x/AS7/logs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export JBOSS_HOME=$HOME/jboss-eap-6.3
-tail -f $JBOSS_HOME/standalone/log/server.log
-
--- deploy/6.1.x/Tomcat7/config/setenv.sh
+++
@@ -1,1 +0,0 @@
-CATALINA_OPTS="-Xmx512M -XX:MaxPermSize=512m -Dbtm.root=$CATALINA_HOME -Dbitronix.tm.configuration=$CATALINA_HOME/conf/btm-config.properties"
