b71bd0b55ce25d338145815024bf0780c59d1f7f
kiereleaseuser <kiereleaseuser@gmail.com>
2015-02-04 16:19:07 +0100

    fixed path problems

--- release/scripts/copyFailedUnitTestsWeb.sh
+++ release/scripts/copyFailedUnitTestsWeb.sh
@@ -1,10 +1,10 @@
-
 #!/bin/bash
 #
 
 TARGET=kiereleaseuser@hp-dl380pg8-01.lab.eng.brq.redhat.com
 FAILED=$HOME/Release
 FAILED_UNIT_TESTS=failedUnitTests
+WORKING_DIR=$HOME/droolsjbpm
 
 echo ""
 echo "Data destination on kiereleaseuser@hp-dl380pg8-01.lab.eng.brq.redhat.com"
@@ -52,6 +52,41 @@ done
 
 ssh $TARGET "cd "$dir"/"$releaseV"; rm -rf "$FAILED_UNIT_TESTS"; mkdir" $FAILED_UNIT_TESTS""
 
-# copy local failedUnitTests directory to TARGET server
+# identify the failed Unit Tests
 
-scp -r $FAILED_TESTS_DIR/* $TARGET:\ /$dir/$releaseV/$FAILED_UNIT_TESTS
+DATE_DIR=$(date +"%d.%m.%Y-%H.%M")
+
+# create directories: local and on the server
+if [ -d "$FAILED_TESTS_DIR" ]; then
+   rm -rf $FAILED_TESTS_DIR
+fi
+
+mkdir $FAILED_TESTS_DIR
+ssh $TARGET "cd "$dir"/"$releaseV"/"$FAILED_UNIT_TESTS"; mkdir" $DATE_DIR""
+
+cd $WORKING_DIR
+rm full_URL.txt
+rm URL.txt
+
+grep -F "Please refer to" $WORKING_DIR/testResult.txt  >> full_URL.txt
+
+FILE_TO_READ=$WORKING_DIR/full_URL.txt
+while read line; do
+  if [ -n "$line" ]; then
+     echo "$line" | cut -f4 -d ' ' >> URL.txt
+  fi
+done < $FILE_TO_READ
+
+
+FILE_TO_READ=$WORKING_DIR/URL.txt
+
+while read line; do
+  if [ -n "$line" ]; then
+     cp $(find $line  -name "*.txt" -exec grep -L "Failures: 0, Errors: 0" {} \;) $FAILED_TESTS_DIR
+  fi
+done < $FILE_TO_READ
+
+
+# copy local failedUnitTests directories: local and to TARGET server
+
+scp $FAILED_TESTS_DIR/* $TARGET:\ /$dir/$releaseV/$FAILED_UNIT_TESTS/$DATE_DIR
