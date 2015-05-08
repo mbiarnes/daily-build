fa4f72be437b59da8eb602908da574bb31b6b0f6
kiereleaseuser <kiereleaseuser@gmail.com>
2015-05-08 16:10:10 +0200

    script modified

--- release/scripts/branchCompare.sh
+++ release/scripts/branchCompare.sh
@@ -1,41 +1,44 @@
 #!/bin/bash
 
+FILE_TO_READ=/home/kiereleaseuser/droolsjbpm/droolsjbpm-build-bootstrap/script/repository-list.txt
+fileDir=/home/kiereleaseuser/droolsjbpm
+
 if [ $# != 2 ] ; then
     echo
     echo "Usage:"
-    echo "  $0  localBranch remoteBranch"
+    echo "  $0 local branch 1 local branch 2"
     echo "For example:"
-    echo "  $0 testBranch master"
+    echo "  $0 bsync-6.2.x-2015.05.07 previousTag"
     echo
     exit 1
 fi
 
-echo "The localBranch is: "$1
-echo "The remoteBranch is: "$2
+echo "droolsjbpm:local branch is: "$1
+echo "previous TAG (local): "$2
+echo "the output file will compare" $1 "vs" $2
 echo -n "Is this ok? (Hit control-c if is not): "
 read ok
 
-localBranch=$1
-remoteBranch=$2
+BRANCH1=$1
+BRANCH2=$2
 
-localFile=$localBranch.txt
-remoteFile=$remoteBranch.txt
+cd $fileDir
 
-rm $localFile
-rm $remoteFile
-
-#echo $localBranch","$remoteBranch","$localFile","$remoteFile
-#exit
-
-
-echo "" >> $localFile
-echo "Local branch ..: $localBranch"  >> $localFile
-sh  $HOME/droolsjbpm/droolsjbpm-build-bootstrap/script/git-all.sh log -5 $localBranch >> $localFile
-
-echo "" >> $remoteFile
-echo "Remote branch ..: $remoteBranch"  >> $remoteFile
-sh $HOME/droolsjbpm/droolsjbpm-build-bootstrap/script/git-all.sh log -5 $remoteBranch >> $remoteFile
-
- 
-
-
+while read line; do
+   if [ -n "$line" ]; then
+      cd $line
+      # gets the last 20 logs of each repo 
+      echo -e "\n**************************************************************" >> $fileDir/diff.txt
+      echo "$line..:" >> $fileDir//diff.txt 
+      echo "$BRANCH1" >> $fileDir/diff.txt
+      git log -20 --pretty=format:%h--%s--%an "$BRANCH1" >> $fileDir/diff.txt
+      echo -e "\n" >> $fileDir/diff.txt
+      echo " --- " >> $fileDir/diff.txt     
+      echo "$BRANCH2" >> $fileDir/diff.txt
+      git log -20 --pretty=format:%h--%s--%an "$BRANCH2" >> $fileDir/diff.txt
+      echo -e "\n" >> $fileDir/diff.txt
+      echo -e "\n" >> $fileDir/diff.txt
+      echo -e "\n" >> $fileDir/diff.txt
+      cd ..
+   fi
+done < $FILE_TO_READ
