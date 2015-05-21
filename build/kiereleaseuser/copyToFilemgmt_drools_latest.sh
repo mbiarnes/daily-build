c9de247c98a6f1237f98d4de580d492793fad1d4
kiereleaseuser <kiereleaseuser@gmail.com>
2015-05-21 18:37:30 +0200

    removed artifacts for jboss.org

--- build/kiereleaseuser/copyToFilemgmt_drools_latest.sh
+++ build/kiereleaseuser/copyToFilemgmt_drools_latest.sh
@@ -6,27 +6,6 @@ export ARTIFACT_DIR=/var/jbpm-artifacts/6.2.x
 
 cd $ARTIFACT_DIR
 
-# dashbuilder
-scp dashbuilder-*-tomcat7.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
-scp dashbuilder-*-was8.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
-scp dashbuilder-*-eap6_4.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
-scp dashbuilder-*-wildfly8.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
-scp dashbuilder-*-weblogic12.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
-
-# jbpm-dashboard
-scp jbpm-dashbuilder-*-tomcat7.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
-scp jbpm-dashbuilder-*-was8.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
-scp jbpm-dashbuilder-*-eap6_4.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
-scp jbpm-dashbuilder-*-wildfly8.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
-scp jbpm-dashbuilder-*-weblogic12.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
-
-# BPMS
-scp kie-wb-*-eap6_4.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
-scp kie-wb-*-tomcat7.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
-scp kie-wb-*-was8.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
-scp kie-wb-*-wildfly8.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
-scp kie-wb-*-weblogic12.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
-
 # BRMS
 scp kie-drools-wb-*-eap6_4.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
 scp kie-drools-wb-*-tomcat7.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
@@ -38,12 +17,6 @@ scp kie-drools-wb-*-weblogic12.war drools@filemgmt.jboss.org:downloads_htdocs/dr
 scp kie-server-*-webc.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
 scp kie-server-*-ee7.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
 scp kie-server-*-ee6.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
-
-# JBPM
-scp jbpm-*-bin.zip drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
-
-# drools-wb-jcr2vfs-distribution-*.zip
-scp drools-wb-jcr2vfs-distribution-*.zip drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
 
 # kie-server-services-*.jar
 scp kie-server-services-*.jar drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/6.2.x
--- build/kiereleaseuser/copyToFilemgmt_drools_master.sh
+++ build/kiereleaseuser/copyToFilemgmt_drools_master.sh
@@ -6,24 +6,6 @@ export ARTIFACT_DIR=/var/jbpm-artifacts/master
 
 cd $ARTIFACT_DIR
 
-# dashbuilder
-    scp dashbuilder-*-tomcat7.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
-    scp dashbuilder-*-was8.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
-    scp dashbuilder-*-eap6_4.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
-    scp dashbuilder-*-wildfly8.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
-    scp dashbuilder-*-weblogic12.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
-# jbpm-dashboard
-    scp jbpm-dashbuilder-*-tomcat7.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
-    scp jbpm-dashbuilder-*-was8.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
-    scp jbpm-dashbuilder-*-eap6_4.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
-    scp jbpm-dashbuilder-*-wildfly8.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
-    scp jbpm-dashbuilder-*-weblogic12.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
-# BPMS
-    scp kie-wb-*-eap6_4.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
-    scp kie-wb-*-tomcat7.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
-    scp kie-wb-*-was8.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
-    scp kie-wb-*-wildfly8.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
-    scp kie-wb-*-weblogic12.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
 # BRMS
     scp kie-drools-wb-*-eap6_4.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
     scp kie-drools-wb-*-tomcat7.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
@@ -34,10 +16,6 @@ cd $ARTIFACT_DIR
     scp kie-server-*-webc.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
     scp kie-server-*-ee7.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
     scp kie-server-*-ee6.war drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
-# JBPM
-    scp jbpm-*-bin.zip drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
-# drools-wb-jcr2vfs-distribution-*.zip
-    scp drools-wb-jcr2vfs-distribution-*.zip drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
 # kie-server-services-*.jar
     scp kie-server-services-*.jar drools@filemgmt.jboss.org:downloads_htdocs/drools/release/snapshot/master
 # kie-server-distribution-*.zip
