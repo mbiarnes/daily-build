0f2898e4da2fbfe607fb265228edf8d4b284a1dc
kiereleaseuser <kiereleaseuser@gmail.com>
2015-05-25 22:36:47 +0200

    fixed re-direction

--- deploy/BPMS_product/startServer.sh
+++ deploy/BPMS_product/startServer.sh
@@ -12,7 +12,7 @@ echo ""
 echo "****************************"
 echo ""
 
-nohup sh $JBOSS_HOME/bin/standalone.sh --server-config=standalone-full.xml&
+nohup sh $JBOSS_HOME/bin/standalone.sh --server-config=standalone-full.xml < /dev/null >> $HOME/scripts/logfile1.log 2>&1 &
 
 echo "exit startServer.sh"
 echo "*******************"
