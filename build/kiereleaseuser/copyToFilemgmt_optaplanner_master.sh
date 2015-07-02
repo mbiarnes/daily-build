3995e74d150a4398f394dd2188f09e2dab11c64a
kiereleaseuser <kiereleaseuser@gmail.com>
2015-07-02 11:46:59 +0200

    renamed artifacts

--- build/kiereleaseuser/copyToFilemgmt_optaplanner_master.sh
+++ build/kiereleaseuser/copyToFilemgmt_optaplanner_master.sh
@@ -2,11 +2,15 @@
 #
 # VARIABLE DEFINITION
 #
+RELEASE=6.3.0-SNAPSHOT
 ARTIFACT_DIR=/var/kie-artifacts/master
+TARGET_SERVER=optaplanner@filemgmt.jboss.org
+TARGET_DIR=downloads_htdocs/optaplanner/release/snapshot/master
+TARGET_DOCS_DIR=docs_htdocs/optaplanner/release/snapshot/master
 
 # OPTAPLANNER
-   scp $ARTIFACT_DIR/optaplanner-distribution-*.zip optaplanner@filemgmt.jboss.org:downloads_htdocs/optaplanner/release/snapshot/master
-   scp -r $ARTIFACT_DIR/docs/optaplanner-docs optaplanner@filemgmt.jboss.org:docs_htdocs/optaplanner/release/snapshot/master
-   scp -r $ARTIFACT_DIR/docs/optaplanner-javadoc optaplanner@filemgmt.jboss.org:docs_htdocs/optaplanner/release/snapshot/master
+   scp $ARTIFACT_DIR/optaplanner-distribution-*.zip $TARGET_SERVER:$TARGET_DIR/optaplanner-distribution-$RELEASE.zip
+   scp -r $ARTIFACT_DIR/docs/optaplanner-docs $TARGET_SERVER:$TARGET_DOCS_DIR
+   scp -r $ARTIFACT_DIR/docs/optaplanner-javadoc $TARGET_SERVER:$TARGET_DOCS_DIR
 
 rm $HOME/scripts/master.txt
