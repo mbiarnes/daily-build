dc7964e690cdc826f99bc78b99173bc82b569a79
kiereleaseuser <kiereleaseuser@gmail.com>
2015-07-07 18:08:55 +0200

    adapted to new artifacts destinations

--- release/scripts/copyArtifacts.sh
+++ release/scripts/copyArtifacts.sh
@@ -11,34 +11,15 @@
 REMOTE_SERVER=@hp-dl380pg8-01.lab.eng.brq.redhat.com
 REMOTE_USER=kiereleaseuser
 REPO_DIR=$HOME/droolsjbpm
+ART_PATH=/var/kie-artifacts
 
-echo ""
-echo "Data destination on kiereleaseuser@hp-dl380pg8-01.lab.eng.brq.redhat.com"
-echo ""
-echo "Destination folder for created artifacts "
-echo "1) master (default)"
-echo "2) 6.1.x"
-echo "3) 6.2.x"
-echo "4) 6.3.x"
-echo ""
-echo "Select 1,2,3 or 4"
-read USR_DIR
-dir=/var/kie-artifacts/master
-case "$USR_DIR" in
-  2) dir=/var/kie-artifacts/6.1.x
-  ;;
-  3) dir=/var/kie-artifacts/6.2.x
-  ;;
-  4) dir=/var/kie-artifacts/6.3.x
-  ;;
-esac
 echo ""
 echo "Release version:"
 echo "i.e. 6.2.0.CR3"
-read releaseV
+read RELEASE_V
 echo ""
-echo "the binaries will be stored in:" $REMOTE_USER$REMOTE_SERVER":"$dir
-echo "the version of the release is:" $releaseV
+echo "the binaries will be stored in:" $REMOTE_USER$REMOTE_SERVER":"$ART_PATH"/"$RELEASE_V
+echo "the version of the release is:" $RELEASE_V
 echo ""
 
 # uploads the artifacts to @hp-dl380pg8-01.lab.eng.brq.redhat.com 
@@ -53,17 +34,15 @@ Please answer yes or no   " yn
     esac
 done
 
-ARTIFACT_DIR=$REMOTE_USER$REMOTE_SERVER:$dir
+ARTIFACT_DIR=$REMOTE_USER$REMOTE_SERVER:$ART_PATH/$RELEASE_V
 
 # creates directory on hp-dl380pg8-01.lab.eng.brq.redhat.com if it is not there
-
-ssh $REMOTE_USER$REMOTE_SERVER "cd /var/kie-artifacts/$dir; rm -rf "$releaseV";  ls -l"
-echo ""
-echo $REMOTE_USER$REMOTE_SERVER "cd /var/kie-artifacts/$dir; rm -rf "$releaseV"; ls -l"
+ssh $REMOTE_USER$REMOTE_SERVER "cd "$ART_PATH"; rm -rf "$RELEASE_V";  ls -l"
  
 touch server-script
-echo "mkdir" $releaseV >> server-script
-sftp -b server-script $ARTIFACT_DIR
+echo "mkdir" $RELEASE_V >> server-script
+sftp -b server-script $REMOTE_USER$REMOTE_SERVER:$ART_PATH
+
 
 # files will be copied to hp-dl380pg8-01.lab.eng.brq.redhat.com
 
@@ -72,98 +51,93 @@ touch server-script
 echo "mkdir examples" >> server-script
 echo "mkdir docs" >> server-script
 
-sftp -b server-script $ARTIFACT_DIR/$releaseV
+sftp -b server-script $ARTIFACT_DIR
 
 rm server-script
-
-ARTIFACT_DIR=$REMOTE_USER$REMOTE_SERVER:
+rm server-script
 
 # dashboard-builder
-scp $REPO_DIR/dashboard-builder/builder/target/dashbuilder-*-tomcat7.war $ARTIFACT_DIR\ $dir/$releaseV
-scp $REPO_DIR/dashboard-builder/builder/target/dashbuilder-*-was8.war $ARTIFACT_DIR\ $dir/$releaseV
-scp $REPO_DIR/dashboard-builder/builder/target/dashbuilder-*-wildfly8.war $ARTIFACT_DIR\ $dir/$releaseV
-scp $REPO_DIR/dashboard-builder/builder/target/dashbuilder-*-eap6_4.war $ARTIFACT_DIR\ $dir/$releaseV
-scp $REPO_DIR/dashboard-builder/builder/target/dashbuilder-*-weblogic12.war $ARTIFACT_DIR\ $dir/$releaseV
+scp $REPO_DIR/dashboard-builder/builder/target/dashbuilder-*-tomcat7.war $ARTIFACT_DIR
+scp $REPO_DIR/dashboard-builder/builder/target/dashbuilder-*-was8.war $ARTIFACT_DIR
+scp $REPO_DIR/dashboard-builder/builder/target/dashbuilder-*-wildfly8.war $ARTIFACT_DIR
+scp $REPO_DIR/dashboard-builder/builder/target/dashbuilder-*-eap6_4.war $ARTIFACT_DIR
+scp $REPO_DIR/dashboard-builder/builder/target/dashbuilder-*-weblogic12.war $ARTIFACT_DIR
 
 # jbpm-dashboard
-scp $REPO_DIR/jbpm-dashboard/jbpm-dashboard-distributions/target/jbpm-dashbuilder-*-tomcat7.war $ARTIFACT_DIR\ $dir/$releaseV
-scp $REPO_DIR/jbpm-dashboard/jbpm-dashboard-distributions/target/jbpm-dashbuilder-*-was8.war $ARTIFACT_DIR\ $dir/$releaseV
-scp $REPO_DIR/jbpm-dashboard/jbpm-dashboard-distributions/target/jbpm-dashbuilder-*-wildfly8.war $ARTIFACT_DIR\ $dir/$releaseV
-scp $REPO_DIR/jbpm-dashboard/jbpm-dashboard-distributions/target/jbpm-dashbuilder-*-eap6_4.war $ARTIFACT_DIR\ $dir/$releaseV
-scp $REPO_DIR/jbpm-dashboard/jbpm-dashboard-distributions/target/jbpm-dashbuilder-*-weblogic12.war $ARTIFACT_DIR\ $dir/$releaseV
+scp $REPO_DIR/jbpm-dashboard/jbpm-dashboard-distributions/target/jbpm-dashbuilder-*-tomcat7.war $ARTIFACT_DIR
+scp $REPO_DIR/jbpm-dashboard/jbpm-dashboard-distributions/target/jbpm-dashbuilder-*-was8.war $ARTIFACT_DIR
+scp $REPO_DIR/jbpm-dashboard/jbpm-dashboard-distributions/target/jbpm-dashbuilder-*-wildfly8.war $ARTIFACT_DIR
+scp $REPO_DIR/jbpm-dashboard/jbpm-dashboard-distributions/target/jbpm-dashbuilder-*-eap6_4.war $ARTIFACT_DIR
+scp $REPO_DIR/jbpm-dashboard/jbpm-dashboard-distributions/target/jbpm-dashbuilder-*-weblogic12.war $ARTIFACT_DIR
 
 # JBPM & droolsjbpm-tools & Optaplanner
-scp $REPO_DIR/jbpm/jbpm-distribution/target/jbpm-*-bin.zip $ARTIFACT_DIR\ $dir/$releaseV
-scp $REPO_DIR/droolsjbpm-tools/drools-eclipse/org.drools.updatesite/target/org.drools.updatesite-*.zip $ARTIFACT_DIR\ $dir/$releaseV
-scp $REPO_DIR/optaplanner/optaplanner-distribution/target/optaplanner-distribution-*.zip $ARTIFACT_DIR\ $dir/$releaseV
+scp $REPO_DIR/jbpm/jbpm-distribution/target/jbpm-*-bin.zip $ARTIFACT_DIR
+scp $REPO_DIR/droolsjbpm-tools/drools-eclipse/org.drools.updatesite/target/org.drools.updatesite-*.zip $ARTIFACT_DIR
+scp $REPO_DIR/optaplanner/optaplanner-distribution/target/optaplanner-distribution-*.zip $ARTIFACT_DIR
 
 # BRMS
-scp $REPO_DIR/kie-wb-distributions/kie-drools-wb/kie-drools-wb-distribution-wars/target/kie-drools-wb-*-wildfly8.war $ARTIFACT_DIR\ $dir/$releaseV
-scp $REPO_DIR/kie-wb-distributions/kie-drools-wb/kie-drools-wb-distribution-wars/target/kie-drools-wb-*-was8.war $ARTIFACT_DIR\ $dir/$releaseV
-scp $REPO_DIR/kie-wb-distributions/kie-drools-wb/kie-drools-wb-distribution-wars/target/kie-drools-wb-*-weblogic12.war $ARTIFACT_DIR\ $dir/$releaseV
-scp $REPO_DIR/kie-wb-distributions/kie-drools-wb/kie-drools-wb-distribution-wars/target/kie-drools-wb-*-eap6_4.war $ARTIFACT_DIR\ $dir/$releaseV
-scp $REPO_DIR/kie-wb-distributions/kie-drools-wb/kie-drools-wb-distribution-wars/target/kie-drools-wb-*-tomcat7.war $ARTIFACT_DIR\ $dir/$releaseV
+scp $REPO_DIR/kie-wb-distributions/kie-drools-wb/kie-drools-wb-distribution-wars/target/kie-drools-wb-*-wildfly8.war $ARTIFACT_DIR
+scp $REPO_DIR/kie-wb-distributions/kie-drools-wb/kie-drools-wb-distribution-wars/target/kie-drools-wb-*-was8.war $ARTIFACT_DIR
+scp $REPO_DIR/kie-wb-distributions/kie-drools-wb/kie-drools-wb-distribution-wars/target/kie-drools-wb-*-weblogic12.war $ARTIFACT_DIR
+scp $REPO_DIR/kie-wb-distributions/kie-drools-wb/kie-drools-wb-distribution-wars/target/kie-drools-wb-*-eap6_4.war $ARTIFACT_DIR
+scp $REPO_DIR/kie-wb-distributions/kie-drools-wb/kie-drools-wb-distribution-wars/target/kie-drools-wb-*-tomcat7.war $ARTIFACT_DIR
 
 # BPMS  
-scp $REPO_DIR/kie-wb-distributions/kie-wb/kie-wb-distribution-wars/target/kie-wb-*-wildfly8.war $ARTIFACT_DIR\ $dir/$releaseV
-scp $REPO_DIR/kie-wb-distributions/kie-wb/kie-wb-distribution-wars/target/kie-wb-*-weblogic12.war $ARTIFACT_DIR\ $dir/$releaseV
-scp $REPO_DIR/kie-wb-distributions/kie-wb/kie-wb-distribution-wars/target/kie-wb-*-was8.war $ARTIFACT_DIR\ $dir/$releaseV
-scp $REPO_DIR/kie-wb-distributions/kie-wb/kie-wb-distribution-wars/target/kie-wb-*-eap6_4.war $ARTIFACT_DIR\ $dir/$releaseV
-scp $REPO_DIR/kie-wb-distributions/kie-wb/kie-wb-distribution-wars/target/kie-wb-*-tomcat7.war $ARTIFACT_DIR\ $dir/$releaseV  
+scp $REPO_DIR/kie-wb-distributions/kie-wb/kie-wb-distribution-wars/target/kie-wb-*-wildfly8.war $ARTIFACT_DIR
+scp $REPO_DIR/kie-wb-distributions/kie-wb/kie-wb-distribution-wars/target/kie-wb-*-weblogic12.war $ARTIFACT_DIR
+scp $REPO_DIR/kie-wb-distributions/kie-wb/kie-wb-distribution-wars/target/kie-wb-*-was8.war $ARTIFACT_DIR
+scp $REPO_DIR/kie-wb-distributions/kie-wb/kie-wb-distribution-wars/target/kie-wb-*-eap6_4.war $ARTIFACT_DIR
+scp $REPO_DIR/kie-wb-distributions/kie-wb/kie-wb-distribution-wars/target/kie-wb-*-tomcat7.war $ARTIFACT_DIR  
   
 # drools examples
-scp $REPO_DIR/droolsjbpm-build-distribution/droolsjbpm-uber-distribution/target/droolsjbpm-uber-distribution-*/downloads_htdocs/drools/release/*/droolsjbpm-integration-*.zip $ARTIFACT_DIR\ $dir/$releaseV/examples
-scp $REPO_DIR/droolsjbpm-build-distribution/droolsjbpm-uber-distribution/target/droolsjbpm-uber-distribution-*/downloads_htdocs/optaplanner/release/*/optaplanner-distribution-*.zip $ARTIFACT_DIR\ $dir/$releaseV/examples
-scp $REPO_DIR/droolsjbpm-build-distribution/droolsjbpm-uber-distribution/target/droolsjbpm-uber-distribution-*/downloads_htdocs/drools/release/*/drools-distribution-*.zip $ARTIFACT_DIR\ $dir/$releaseV/examples
+scp $REPO_DIR/droolsjbpm-build-distribution/droolsjbpm-uber-distribution/target/droolsjbpm-uber-distribution-*/downloads_htdocs/drools/release/*/droolsjbpm-integration-*.zip $ARTIFACT_DIR/examples
+scp $REPO_DIR/droolsjbpm-build-distribution/droolsjbpm-uber-distribution/target/droolsjbpm-uber-distribution-*/downloads_htdocs/optaplanner/release/*/optaplanner-distribution-*.zip $ARTIFACT_DIR/examples
+scp $REPO_DIR/droolsjbpm-build-distribution/droolsjbpm-uber-distribution/target/droolsjbpm-uber-distribution-*/downloads_htdocs/drools/release/*/drools-distribution-*.zip $ARTIFACT_DIR/examples
 
 # kie-tomcat-integration
-scp $REPO_DIR/kie-wb-distributions/kie-tomcat-integration/target/kie-tomcat-integration-*.jar $ARTIFACT_DIR\ $dir/$releaseV
+scp $REPO_DIR/kie-wb-distributions/kie-tomcat-integration/target/kie-tomcat-integration-*.jar $ARTIFACT_DIR
 
 # copy examples to $HOME/RELEASE/<release>/drools-uber-distribution
 
-mkdir -p $HOME/Release/$releaseV/droolsjbpm-uber-distribution
-EXAMPLE_DIR=$HOME/Release/$releaseV/droolsjbpm-uber-distribution
+mkdir -p $HOME/Release/$REALEASE_V/droolsjbpm-uber-distribution
+EXAMPLE_DIR=$HOME/Release/$RELEASE_V/droolsjbpm-uber-distribution
 
 cp $REPO_DIR/droolsjbpm-build-distribution/droolsjbpm-uber-distribution/target/droolsjbpm-uber-distribution-*/downloads_htdocs/drools/release/*/drools-distribution-*.zip $EXAMPLE_DIR
 cp $REPO_DIR/droolsjbpm-build-distribution/droolsjbpm-uber-distribution/target/droolsjbpm-uber-distribution-*/downloads_htdocs/drools/release/*/droolsjbpm-integration-*.zip $EXAMPLE_DIR
 cp $REPO_DIR/droolsjbpm-build-distribution/droolsjbpm-uber-distribution/target/droolsjbpm-uber-distribution-*/downloads_htdocs/optaplanner/release/*/optaplanner-distribution-*.zip $EXAMPLE_DIR
 
 # kie-server-services-*.jar
-scp $REPO_DIR/droolsjbpm-integration/kie-server-parent/kie-server-services/target/kie-server-services-*.jar $ARTIFACT_DIR\ $dir/$releaseV
+scp $REPO_DIR/droolsjbpm-integration/kie-server-parent/kie-server-services/kie-server-services-drools/target/kie-server-services*.jar
+scp $REPO_DIR/droolsjbpm-integration/kie-server-parent/kie-server-services/kie-server-services-jbpm/target/kie-server-services*.jar
 
 #kie-server-distribution-*.zip
-scp $REPO_DIR/droolsjbpm-integration/kie-server-parent/kie-server-distribution/target/kie-server-distribution-*.zip $ARTIFACT_DIR\ $dir/$releaseV
+scp $REPO_DIR/droolsjbpm-integration/kie-server-parent/kie-server-wars/kie-server-distribution/target/kie-server-distribution-*.zip $ARTIFACT_DIR
 
 #kie-server*.war
-scp $REPO_DIR/droolsjbpm-integration/kie-server-parent/kie-server/target/kie-server*.war $ARTIFACT_DIR\ $dir/$releaseV
+scp $REPO_DIR/droolsjbpm-integration/kie-server-parent/kie-server-wars/kie-server/target/kie-server*.war $ARTIFACT_DIR
 
 # drools-wb-jcr2vfs-distribution-*.zip
-scp $REPO_DIR/drools-wb/drools-wb-jcr2vfs-migration/drools-wb-jcr2vfs-distribution/target/drools-wb-jcr2vfs-distribution-*.zip $ARTIFACT_DIR\ $dir/$releaseV
+scp $REPO_DIR/drools-wb/drools-wb-jcr2vfs-migration/drools-wb-jcr2vfs-distribution/target/drools-wb-jcr2vfs-distribution-*.zip $ARTIFACT_DIR
 
 # remove superfluous artifacts
 
-ssh $REMOTE_USER$REMOTE_SERVER "cd "$dir"/"$releaseV"; rm kie-server-services-*-javadoc.jar"
-ssh $REMOTE_USER$REMOTE_SERVER "cd "$dir"/"$releaseV"; rm kie-server-services-*-sources.jar"
-ssh $REMOTE_USER$REMOTE_SERVER "cd "$dir"/"$releaseV"; rm kie-server-services-*-tests.jar"
-ssh $REMOTE_USER$REMOTE_SERVER "cd "$dir"/"$releaseV"; rm kie-tomcat-integration-*-javadoc.jar"
-ssh $REMOTE_USER$REMOTE_SERVER "cd "$dir"/"$releaseV"; rm kie-tomcat-integration-*-sources.jar"
-ssh $REMOTE_USER$REMOTE_SERVER "cd "$dir"/"$releaseV"; rm kie-tomcat-integration-*-tests.jar"
-
-
-
+ssh $REMOTE_USER$REMOTE_SERVER "cd "$ART_PATH"/"$RELEASE_V"; rm kie-server-services-*-javadoc.jar"
+ssh $REMOTE_USER$REMOTE_SERVER "cd "$ART_PATH"/"$RELEASE_V"; rm kie-server-services-*-sources.jar"
+ssh $REMOTE_USER$REMOTE_SERVER "cd "$ART_PATH"/"$RELEASE_V"; rm kie-server-services-*-tests.jar"
+ssh $REMOTE_USER$REMOTE_SERVER "cd "$ART_PATH"/"$RELEASE_V"; rm kie-server-*-tests.jar"
 
 # copy documentation
 
-ssh $REMOTE_USER$REMOTE_SERVER "cd "$dir"/"$releaseV"/docs; mkdir drools-docs"
-scp -r $REPO_DIR/kie-wb-distributions/kie-docs/drools-docs/target/docbook/publish/en-US/* $ARTIFACT_DIR\ $dir/$releaseV/docs/drools-docs
-ssh $REMOTE_USER$REMOTE_SERVER "cd "$dir"/"$releaseV"/docs; mkdir jbpm-docs"
-scp -r $REPO_DIR/kie-wb-distributions/kie-docs/jbpm-docs/target/docbook/publish/en-US/* $ARTIFACT_DIR\ $dir/$releaseV/docs/jbpm-docs
-ssh $REMOTE_USER$REMOTE_SERVER "cd "$dir"/"$releaseV"/docs; mkdir dashbuilder-docs"
-scp -r $REPO_DIR/kie-wb-distributions/kie-docs/dashbuilder-docs/target/docbook/publish/en-US/* $ARTIFACT_DIR\ $dir/$releaseV/docs/dashbuilder-docs
-ssh $REMOTE_USER$REMOTE_SERVER "cd "$dir"/"$releaseV"/docs; mkdir optaplanner-docs"
-scp -r $REPO_DIR/optaplanner/optaplanner-docs/target/docbook/publish/en-US/* $ARTIFACT_DIR\ $dir/$releaseV/docs/optaplanner-docs
-ssh $REMOTE_USER$REMOTE_SERVER "cd "$dir"/"$releaseV"/docs; mkdir optaplanner-javadoc"
-scp -r $REPO_DIR/droolsjbpm-build-distribution/droolsjbpm-uber-distribution/target/droolsjbpm-uber-distribution-*/docs_htdocs/optaplanner/release/*/optaplanner-javadoc/* $ARTIFACT_DIR\ $dir/$releaseV/docs/optaplanner-javadoc
-ssh $REMOTE_USER$REMOTE_SERVER "cd "$dir"/"$releaseV"/docs; mkdir kie-api-javadoc"
-scp -r $REPO_DIR/droolsjbpm-build-distribution/droolsjbpm-uber-distribution/target/droolsjbpm-uber-distribution-*/docs_htdocs/drools/release/*/kie-api-javadoc/* $ARTIFACT_DIR\ $dir/$releaseV/docs/kie-api-javadoc
+ssh $REMOTE_USER$REMOTE_SERVER "cd "$ART_PATH"/"$RELEASE_V"/docs; mkdir drools-docs"
+scp -r $REPO_DIR/kie-wb-distributions/kie-docs/drools-docs/target/docbook/publish/en-US/* $ARTIFACT_DIR/docs/drools-docs
+ssh $REMOTE_USER$REMOTE_SERVER "cd "$ART_PATH"/"$RELEASE_V"/docs; mkdir jbpm-docs"
+scp -r $REPO_DIR/kie-wb-distributions/kie-docs/jbpm-docs/target/docbook/publish/en-US/* $ARTIFACT_DIR/docs/jbpm-docs
+ssh $REMOTE_USER$REMOTE_SERVER "cd "$ART_PATH"/"$RELEASE_V"/docs; mkdir dashbuilder-docs"
+scp -r $REPO_DIR/kie-wb-distributions/kie-docs/dashbuilder-docs/target/docbook/publish/en-US/* $ARTIFACT_DIR/docs/dashbuilder-docs
+ssh $REMOTE_USER$REMOTE_SERVER "cd "$ART_PATH"/"$RELEASE_V"/docs; mkdir optaplanner-docs"
+scp -r $REPO_DIR/optaplanner/optaplanner-docs/target/docbook/publish/en-US/* $ARTIFACT_DIR/docs/optaplanner-docs
+ssh $REMOTE_USER$REMOTE_SERVER "cd "$ART_PATH"/"$RELEASE_V"/docs; mkdir optaplanner-javadoc"
+scp -r $REPO_DIR/droolsjbpm-build-distribution/droolsjbpm-uber-distribution/target/droolsjbpm-uber-distribution-*/docs_htdocs/optaplanner/release/*/optaplanner-javadoc/* $ARTIFACT_DIR/docs/optaplanner-javadoc
+ssh $REMOTE_USER$REMOTE_SERVER "cd "$ART_PATH"/"$RELEASE_V"/docs; mkdir kie-api-javadoc"
+scp -r $REPO_DIR/droolsjbpm-build-distribution/droolsjbpm-uber-distribution/target/droolsjbpm-uber-distribution-*/docs_htdocs/drools/release/*/kie-api-javadoc/* $ARTIFACT_DIR/docs/kie-api-javadoc
 
--- release/scripts/copyFailedUnitTestsWeb.sh
+++ release/scripts/copyFailedUnitTestsWeb.sh
@@ -7,34 +7,12 @@ FAILED_UNIT_TESTS=failedUnitTests
 WORKING_DIR=$HOME/droolsjbpm
 
 echo ""
-echo "Data destination on kiereleaseuser@hp-dl380pg8-01.lab.eng.brq.redhat.com"
-echo "for the failed Unit Tests:"
-echo ""
-echo a"1) master (default)"
-echo "2) 6.1.x"
-echo "3) 6.2.x"
-echo "4) 6.3.x"
-echo ""
-echo "Select 1,2,3 or 4"
-read USR_DIR
-dir=/var/kie-artifacts/master
-case "$USR_DIR" in
-  2) dir=/var/kie-artifacts/6.1.x
-  ;;
-  3) dir=/var/kie-artifacts/6.2.x
-  ;;
-  4) dir=/var/kie-artifacts/6.3.x
-  ;;
-esac
-echo ""
 echo "Release version:"
 echo "i.e. 6.2.0.CR3"
-read releaseV
+read RELEASE_V
 echo ""
-echo "the failedUnit tests will be copied from :" $FAILED"/"$releaseV"/"$FAILED_TEST_DIR" to " $TARGET":"$dir"/"$releaseV"/"$FAILED_UNIT_TESTS
+echo "the release version is: "$RELEASE_V
 echo ""
-
-FAILED_TESTS_DIR=$FAILED/$releaseV/$FAILED_UNIT_TESTS
 
 # uploads the failed Unit Tests to @hp-dl380pg8-01.lab.eng.brq.redhat.com 
 echo ""
@@ -48,13 +26,15 @@ Please answer yes or no " yn
     esac
 done
 
+TARGET_DIR=/var/kie-artifacts/$RELEASE_V
 # create directory failedUnitTests on TARGET server
 
-ssh $TARGET "cd "$dir"/"$releaseV"; rm -rf "$FAILED_UNIT_TESTS"; mkdir" $FAILED_UNIT_TESTS""
+ssh $TARGET "cd "$TARGET_DIR"; rm -rf "$FAILED_UNIT_TESTS"; mkdir" $FAILED_UNIT_TESTS""
 
 # identify the failed Unit Tests
 
 DATE_DIR=$(date +"%d.%m.%Y-%H.%M")
+FAILED_TESTS_DIR=$FAILED/$RELEASE_V/$FAILED_UNIT_TESTS
 
 # create directories: local and on the server
 if [ -d "$FAILED_TESTS_DIR" ]; then
@@ -62,7 +42,9 @@ if [ -d "$FAILED_TESTS_DIR" ]; then
 fi
 
 mkdir $FAILED_TESTS_DIR
-ssh $TARGET "cd "$dir"/"$releaseV"/"$FAILED_UNIT_TESTS"; mkdir" $DATE_DIR""
+mkdir $FAILED_TESTS_DIR/$DATE_DIR
+
+ssh $TARGET "cd "$TARGET_DIR"/"$FAILED_UNIT_TESTS"; mkdir" $DATE_DIR""
 
 cd $WORKING_DIR
 rm full_URL.txt
@@ -82,11 +64,11 @@ FILE_TO_READ=$WORKING_DIR/URL.txt
 
 while read line; do
   if [ -n "$line" ]; then
-     cp $(find $line  -name "*.txt" -exec grep -L "Failures: 0, Errors: 0" {} \;) $FAILED_TESTS_DIR
+     cp $(find $line  -name "*.txt" -exec grep -L "Failures: 0, Errors: 0" {} \;) $FAILED_TESTS_DIR/$DATE_DIR
   fi
 done < $FILE_TO_READ
 
 
 # copy local failedUnitTests directories: local and to TARGET server
 
-scp $FAILED_TESTS_DIR/* $TARGET:\ /$dir/$releaseV/$FAILED_UNIT_TESTS/$DATE_DIR
+scp $FAILED_TESTS_DIR/$DATE_DIR/* $TARGET:$TARGET_DIR/$FAILED_UNIT_TESTS/$DATE_DIR
