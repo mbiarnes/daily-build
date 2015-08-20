65dccf2845bc8c54a96b87878b51373e6349acf9
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2015-08-20 11:11:56 +0200

    finished script

--- release/scripts/copyFailedUnitTestsWebTo-kie-releases.sh
+++ release/scripts/copyFailedUnitTestsWebTo-kie-releases.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
 
-
 echo ""
 echo "Release version:"
 echo "i.e. 6.2.0.CR3"
@@ -14,39 +13,26 @@ read ok
 
 
 TARGET=kiereleaseuser@hp-dl380pg8-01.lab.eng.brq.redhat.com
-RELEASE_DIR=$HOME/Release
-FAILED_UNIT_TESTS=failedUnitTests
+TEST_DIR=failedUnitTests
 WORKING_DIR=$HOME/droolsjbpm
-FAILED_TESTS_DIR=$FAILED/$releaseV/$FAILED_UNIT_TESTS
-
-# uploads the failed Unit Tests to @hp-dl380pg8-01.lab.eng.brq.redhat.com 
-echo ""
-while true; do
-    read -p "Do you wish to upload failed unit tests to "$TARGET" ?
-Please answer yes or no " yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-# create directory failedUnitTests on TARGET server
-
-ssh $TARGET "cd "$dir"/"$releaseV"; rm -rf "$FAILED_UNIT_TESTS"; mkdir" $FAILED_UNIT_TESTS""
-
-# identify the failed Unit Tests
-
+RELEASE_DIR=$HOME/Release/$releaseV/$TEST_DIR
+KIE_DIR=/var/kie-artifacts/$releaseV/$TEST_DIR
 DATE_DIR=$(date +"%d.%m.%Y-%H.%M")
 
-# create directories: local and on the server
-if [ -d "$FAILED_TESTS_DIR" ]; then
-   rm -rf $FAILED_TESTS_DIR
+
+# create directories in Release directory and kie-artifacts/<release> directory
+if [ -d "$RELEASE_DIR" ]; then
+   rm -rf $REALEASE_DIR
 fi
+mkdir -p $RELEASE_DIR
 
-mkdir $FAILED_TESTS_DIR
-ssh $TARGET "cd "$dir"/"$releaseV"/"$FAILED_UNIT_TESTS"; mkdir" $DATE_DIR""
+if [ -d "$KIE_DIR" ]; then
+   rm -rf $KIE_DIR
+fi
+mkdir -p $KIE_DIR
+mkdir -p $KIE_DIR/$DATE_DIR
 
+# scan for failed UnitTests in Result.txt
 cd $WORKING_DIR
 rm full_URL.txt
 rm URL.txt
@@ -65,11 +51,11 @@ FILE_TO_READ=$WORKING_DIR/URL.txt
 
 while read line; do
   if [ -n "$line" ]; then
-     cp $(find $line  -name "*.txt" -exec grep -L "Failures: 0, Errors: 0" {} \;) $FAILED_TESTS_DIR
+     cp $(find $line  -name "*.txt" -exec grep -L "Failures: 0, Errors: 0" {} \;) $RELEASE_DIR
   fi
 done < $FILE_TO_READ
 
 
-# copy local failedUnitTests directories: local and to TARGET server
+# copy failedUnitTests stored in Release to kie-artifacts directory
 
-scp $FAILED_TESTS_DIR/* $TARGET:\ /$dir/$releaseV/$FAILED_UNIT_TESTS/$DATE_DIR
+cp $RELEASE_DIR/* $KIE_DIR/$DATE_DIR
